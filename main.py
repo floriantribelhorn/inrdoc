38f096ec929637fd7876734f1161e12ab59ea76c
floriantribelhorn <161645581+floriantribelhorn@users.noreply.github.com>
2024-03-27 19:19:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ if 'loginstatus' not in st.session_state:
 if 'loggedinuser' not in st.session_state:
     st.session_state['loggedinuser'] = False
 
+if 'loggedinuserid' not in st.session_state:
+    st.session_state['loggedinuserid'] = False
+
 if __name__ == '__main__':
     main(st.session_state['loginstatus'])
     user_database()
