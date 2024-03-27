3ec29dbe1f1c2f64dd05cbfed6f4758e1b68de4b
floriantribelhorn <161645581+floriantribelhorn@users.noreply.github.com>
2024-03-27 18:36:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import numpy as np
+from functions.database_new import *
+from functions.functions1_new import *
+
+if 'loginstatus' not in st.session_state:
+    st.session_state['loginstatus'] = False
+
+if 'loggedinuser' not in st.session_state:
+    st.session_state['loggedinuser'] = False
+
+if __name__ == '__main__':
+    main(st.session_state['loginstatus'])
+    user_database()
+    setup_quickdatabase(st.session_state['loggedinuser'])
---
+++ pages/editor.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from functions.functions1_new import *
+from functions.database_new import *
+
+if 'loginstatus' not in st.session_state:
+    st.session_state['loginstatus'] = False
+
+if __name__ == '__main__':
+    main(st.session_state['loginstatus'])
+    jahre = list()
+    for i in range(100):
+        if i < 10:
+            jahre.append('190'+str(i))
+        else:
+            jahre.append('19'+str(i))
+    for e in range(25):
+        if e < 10:
+            jahre.append('200'+str(e))
+        else:
+            jahre.append('20'+str(e))
+    jahre.reverse()
+    with st.container(border=True):
+        bereich = st.selectbox('Wählen Sie das gewünschte Jahr aus',(jahre))
+    editoranzeige(st.session_state['loggedinuser'],bereich)
