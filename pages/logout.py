3e3a08e5ef7304f4e0c61681941f0352b9a79696
floriantribelhorn <161645581+floriantribelhorn@users.noreply.github.com>
2024-03-28 12:16:53 +0100

    Delete pages directory

--- pages/logout.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import numpy as np
-from functions.database_new import *
-from functions.functions1_new import *
-
-st.session_state['loginstatus'] = False
-st.session_state['loggedinuser'] = False
-st.switch_page('main.py')
