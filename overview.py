119deb2fc1db2a7b48fe240e429232f1504790c8
floriantribelhorn <161645581+floriantribelhorn@users.noreply.github.com>
2024-03-28 12:37:24 +0100

    Add files via upload

---
+++ overview.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from functions.functions1_new import *
+from functions.database_new import *
+
+if 'loginstatus' not in st.session_state:
+    st.session_state['loginstatus'] = False
+
+if __name__ == '__main__':
+    main(st.session_state['loginstatus'])
+
+if st.session_state['loginstatus'] != False:
+    with st.container(border=True):
+        zeit = st.selectbox('Quick-Werte Zeitraum:', ('5 Tage','10 Tage', '30 Tage', '2 Monate','6 Monate','1 Jahr'))
+        zeigen = st.button('Werte ausgeben')
+
+    if zeigen:
+        dauer = zeitraum(zeit)
+        quick_data_check(st.session_state['loggedinuserid'],dauer)
