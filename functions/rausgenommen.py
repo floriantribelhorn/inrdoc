e0bb7a55598cb7f8d99c374c7e0c58f34c0ddcdd
floriantribelhorn <161645581+floriantribelhorn@users.noreply.github.com>
2024-05-16 19:32:30 +0200

    Delete functions/rausgenommen.py

--- functions/rausgenommen.py
+++
@@ -1,6 +0,0 @@
-                if alles:
-                    nichtgewählte = [key for key, checked in checked_boxes.items() if not checked]
-                    st.success(nichtgewählte)
-                    for state in nichtgewählte:
-                        st.session_state[f'{state}'] = True
-                        st.write(st.session_state[f'{state}'])
