240e5b3666c2a77f1fb1ebba4b97c2c3a9dfe0ee
floriantribelhorn <161645581+floriantribelhorn@users.noreply.github.com>
2024-05-16 13:12:57 +0200

    Add files via upload

--- pages/messeingabe.py
+++ pages/messeingabe.py
@@ -16,32 +16,24 @@ st.session_state['aktuell'] = 'Dateneingabe'
 if __name__ == '__main__':
     main(st.session_state['loginstatus'])
 
-conn = mysql.connector.connect(**connex())
-cursor = conn.cursor()
-cursor.execute('SELECT `device_name`,`device_nr` FROM `freedb_inrdoc`.`devices`')
-rows = cursor.fetchall()
-device_dict = {}
-for name, nr in rows:
-    device_dict[name] = nr
-conn.commit()
-conn.close()
-
-conn = mysql.connector.connect(**connex())
-cursor = conn.cursor()
-cursor.execute('SELECT `lotnr`,`expiry` FROM `freedb_inrdoc`.`lot_data`')
-rows = cursor.fetchall()
-lot_dict = {}
-for name, nr in rows:
-    lot_dict[name] = nr
-conn.commit()
-conn.close()
-
 if st.session_state['loginstatus'] != False:
+    conn = mysql.connector.connect(**connex())
+    cursor = conn.cursor()
+    cursor.execute('SELECT `device_name`,`device_nr` FROM `freedb_inrdoc`.`devices`')
+    rows = cursor.fetchall()
+    device_dict = {}
+    for name, nr in rows:
+        device_dict[name] = nr
+    conn.close()
+    mydevicelist = list(device_dict.keys())
+    nr = mydevice(st.session_state['loggedinuserid'])
+    mydev = mydevicelist[nr-1]
+
     if st.session_state['medikament'] == True:
         with st.expander(label='aktuelle Messumgebung (Gerät/LOT-NR. des Reagenz)',expanded=False):
             st.subheader('aktuelle Messumgebung')
-            geraet = st.selectbox(label='Messgerät',options=device_dict,index=1)
-            lot = st.selectbox(label='aktuelle LOT',options=lot_dict,index=1)
+            st.text(mydev)
+            lot = st.selectbox(label='aktuelle LOT',options=['o1','o1','o3'])
         with st.expander(label='Dateneingabe',expanded=False):
             st.subheader('Quick-Eingabe')
             heutigerquick = st.number_input(label = "Quick-Messwert", min_value=0, max_value=130, step=1)
--- pages/update.py
+++ pages/update.py
@@ -3,22 +3,43 @@ from functions.utilities import *
 from functions.user_functions import *
 from functions.quick_functions import *
 from functions.cnx import *
+import time
 
 conn = mysql.connector.connect(**connex())
 cursor = conn.cursor()
-cursor.execute('SELECT `device_name`,`device_nr` FROM `freedb_inrdoc`.`devices`')
+cursor.execute('SELECT `id`,`lotnr`,`device` FROM `freedb_inrdoc`.`lot_numbers`')
 rows = cursor.fetchall()
-device_dict = {}
-for name, nr in rows:
-    device_dict[name] = nr
-conn.commit()
+dict1 = {}
+dict2 = {}
+dict3 = {}
+for id, lot_nr, device in rows:
+    if device == 1:
+        dict1[id] = lot_nr
+    elif device == 2:
+        dict2[id] = lot_nr
+    else:
+        dict3[id] = lot_nr
+conn.close()
+
+conn = mysql.connector.connect(**connex())
+cursor = conn.cursor()
+cursor.execute(f'SELECT `device_data`.`new_device`  FROM `device_data` WHERE `user` = %s ORDER BY `device_data`.`updated` DESC LIMIT 1''',(st.session_state['loggedinuserid'],))
+rows = cursor.fetchone()
+if rows:
+    current_device = rows[0]
 conn.close()
 
 with st.container(border=True):
-    with st.form(key='deviceupdate',clear_on_submit=True,border=True):
-        st.subheader('In Ihrem Profil wurde noch kein Messgerät erfasst!')
-        dev = st.selectbox(label='Medikament',options=list(device_dict.keys()),placeholder='Wählen Sie Ihr derzeitiges Messgerät aus!')
-        device = device_dict[dev]
-        st.form_submit_button(label='Abspeichern',on_click=device_update, args=(device,))
-if st.button('Weiterfahren'):
-    st.switch_page('main.py')
+    with st.form(key='lotupdate',clear_on_submit=True,border=True):
+        st.subheader('In Ihrem Profil wurde noch keine Lot-Nr. des Reagenz erfasst!')
+        if current_device == 1:
+            lot = st.selectbox(label='LOT-Nr.',options=list(dict1.values()),placeholder='Wählen Sie Ihre aktuelle LOT-Nr. aus!')
+        elif current_device == 2:  
+            lot = st.selectbox(label='LOT-Nr.',options=list(dict2.values()),placeholder='Wählen Sie Ihre aktuelle LOT-Nr. aus!')
+        else:
+            lot = st.selectbox(label='LOT-Nr.',options=list(dict3.values()),placeholder='Wählen Sie Ihre aktuelle LOT-Nr. aus!')
+        st.form_submit_button(label='Abspeichern',on_click=lot_update1, args=(lot,))
+        click = st.button(label='Weiterfahren zur App')
+        if click:
+            time.sleep(3)
+            st.switch_page('main.py')
