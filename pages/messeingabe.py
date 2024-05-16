21cd94de4d66a0d70076d67e657de280c4882553
floriantribelhorn <161645581+floriantribelhorn@users.noreply.github.com>
2024-05-16 19:33:31 +0200

    Add files via upload

--- pages/messeingabe.py
+++ pages/messeingabe.py
@@ -17,37 +17,47 @@ if __name__ == '__main__':
     main(st.session_state['loginstatus'])
 
 if st.session_state['loginstatus'] != False:
-    conn = mysql.connector.connect(**connex())
-    cursor = conn.cursor()
-    cursor.execute('SELECT `device_name`,`device_nr` FROM `freedb_inrdoc`.`devices`')
-    rows = cursor.fetchall()
-    device_dict = {}
-    for name, nr in rows:
-        device_dict[name] = nr
-    conn.close()
-    mydevicelist = list(device_dict.keys())
-    nr = mydevice(st.session_state['loggedinuserid'])
-    mydev = mydevicelist[nr-1]
-
     if st.session_state['medikament'] == True:
-        with st.expander(label='aktuelle Messumgebung (Gerät/LOT-NR. des Reagenz)',expanded=False):
-            st.subheader('aktuelle Messumgebung')
-            st.text(mydev)
-            lot = st.selectbox(label='aktuelle LOT',options=['o1','o1','o3'])
-        with st.expander(label='Dateneingabe',expanded=False):
-            st.subheader('Quick-Eingabe')
-            heutigerquick = st.number_input(label = "Quick-Messwert", min_value=0, max_value=130, step=1)
-            datum = st.date_input('Zeitpunkt der Messung',format='DD/MM/YYYY')
-            abspeichern = st.button('Quick jetzt abspeichern')
-            if abspeichern:
-                if futuredate(datum) == False:
-                    if not quick_empty(st.session_state['loggedinuserid'],datum):
-                        quick_eintrag(heutigerquick,datum)
+        conn = mysql.connector.connect(**connex())
+        cursor = conn.cursor()
+        cursor.execute('SELECT `device_name`,`device_nr` FROM `freedb_inrdoc`.`devices`')
+        rows = cursor.fetchall()
+        device_dict = {}
+        for name, nr in rows:
+            device_dict[name] = nr
+        conn.close()
+        mydevicelist = list(device_dict.keys())
+        nr = mydevice(st.session_state['loggedinuserid'])
+        mydev = mydevicelist[nr-1]
+        user = st.session_state['loggedinuserid']
+        conn = mysql.connector.connect(**connex())
+        cursor = conn.cursor()
+        cursor.execute(f'SELECT `lot_numbers`.`lotnr`,`lot_numbers`.`expiry` FROM `freedb_inrdoc`.`lot_numbers` JOIN `lot_data` ON `lot_data`.`new_lot` = `lot_numbers`.`id` WHERE `lot_data`.`user` = %s ORDER BY `lot_data`.`updated` DESC LIMIT 1',(user,))
+        rows = cursor.fetchone()
+        current_lot = rows[0]
+        expiry = rows[1]
+        conn.close()
+        if is_expired(str(expiry)):
+            st.warning('Ihre aktuelles Reagenz hat das Verfalldatum überschritten!')
+        else:
+            with st.expander(label='aktuelle Messumgebung (Gerät/LOT-NR. des Reagenz)',expanded=False):
+                st.subheader('aktuelle Messumgebung')
+                st.text(mydev)
+                st.text(current_lot)
+            with st.expander(label='Dateneingabe',expanded=False):
+                st.subheader('Quick-Eingabe')
+                heutigerquick = st.number_input(label = "Quick-Messwert", min_value=0, max_value=130, step=1)
+                datum = st.date_input('Zeitpunkt der Messung',format='DD/MM/YYYY')
+                abspeichern = st.button('Quick jetzt abspeichern')
+                if abspeichern:
+                    if futuredate(datum) == False:
+                        if not quick_empty(st.session_state['loggedinuserid'],datum):
+                            quick_eintrag(heutigerquick,datum)
+                        else:
+                            datum1 = datum.strftime('%d-%m-%Y')
+                            st.info(f"Am {datum1} existiert bereits eine eingetragene Messung.")
                     else:
-                        datum1 = datum.strftime('%d-%m-%Y')
-                        st.info(f"Am {datum1} existiert bereits eine eingetragene Messung.")
-                else:
-                    st.info('Sie versuchen den Messwert an einem zukünftigen Datum abzuspeichern!')
+                        st.info('Sie versuchen den Messwert an einem zukünftigen Datum abzuspeichern!')
     else:
         st.text('Noch kein Medikament erfasst!')
 else:
--- pages/update.py
+++ pages/update.py
@@ -7,39 +7,46 @@ import time
 
 conn = mysql.connector.connect(**connex())
 cursor = conn.cursor()
-cursor.execute('SELECT `id`,`lotnr`,`device` FROM `freedb_inrdoc`.`lot_numbers`')
-rows = cursor.fetchall()
-dict1 = {}
-dict2 = {}
-dict3 = {}
-for id, lot_nr, device in rows:
-    if device == 1:
-        dict1[id] = lot_nr
-    elif device == 2:
-        dict2[id] = lot_nr
-    else:
-        dict3[id] = lot_nr
-conn.close()
-
-conn = mysql.connector.connect(**connex())
-cursor = conn.cursor()
 cursor.execute(f'SELECT `device_data`.`new_device`  FROM `device_data` WHERE `user` = %s ORDER BY `device_data`.`updated` DESC LIMIT 1''',(st.session_state['loggedinuserid'],))
 rows = cursor.fetchone()
 if rows:
     current_device = rows[0]
 conn.close()
 
-with st.container(border=True):
-    with st.form(key='lotupdate',clear_on_submit=True,border=True):
-        st.subheader('In Ihrem Profil wurde noch keine Lot-Nr. des Reagenz erfasst!')
-        if current_device == 1:
-            lot = st.selectbox(label='LOT-Nr.',options=list(dict1.values()),placeholder='Wählen Sie Ihre aktuelle LOT-Nr. aus!')
-        elif current_device == 2:  
-            lot = st.selectbox(label='LOT-Nr.',options=list(dict2.values()),placeholder='Wählen Sie Ihre aktuelle LOT-Nr. aus!')
+if not rows:
+    with st.container(border=True):
+        with st.form(key='deviceupdate',clear_on_submit=False,border=True):
+            st.subheader('In Ihrem Profil wurde noch kein Gerät erfasst!')
+            dev = st.selectbox(label='Messgerät',options=list(device_dict.keys()),placeholder='Wählen Sie Ihr Messgerät aus!')
+            st.form_submit_button(label='Abspeichern',on_click=device_update2, args=(dev,))
+else:
+    conn = mysql.connector.connect(**connex())
+    cursor = conn.cursor()
+    cursor.execute('SELECT `id`,`lotnr`,`device` FROM `freedb_inrdoc`.`lot_numbers`')
+    rows = cursor.fetchall()
+    dict1 = {}
+    dict2 = {}
+    dict3 = {}
+    for id, lot_nr, device in rows:
+        if device == 1:
+            dict1[id] = lot_nr
+        elif device == 2:
+            dict2[id] = lot_nr
         else:
-            lot = st.selectbox(label='LOT-Nr.',options=list(dict3.values()),placeholder='Wählen Sie Ihre aktuelle LOT-Nr. aus!')
-        st.form_submit_button(label='Abspeichern',on_click=lot_update1, args=(lot,))
+            dict3[id] = lot_nr
+    conn.close()
+
+    with st.container(border=True):
+        with st.form(key='lotupdate',clear_on_submit=True,border=True):
+            st.subheader('In Ihrem Profil wurde noch keine Lot-Nr. des Reagenz erfasst!')
+            if current_device == 1:
+                lot = st.selectbox(label='LOT-Nr.',options=list(dict1.values()),placeholder='Wählen Sie Ihre aktuelle LOT-Nr. aus!')
+            elif current_device == 2:  
+                lot = st.selectbox(label='LOT-Nr.',options=list(dict2.values()),placeholder='Wählen Sie Ihre aktuelle LOT-Nr. aus!')
+            else:
+                lot = st.selectbox(label='LOT-Nr.',options=list(dict3.values()),placeholder='Wählen Sie Ihre aktuelle LOT-Nr. aus!')
+            st.form_submit_button(label='Abspeichern',on_click=lot_update1, args=(lot,))
         click = st.button(label='Weiterfahren zur App')
         if click:
-            time.sleep(3)
+            time.sleep(1)
             st.switch_page('main.py')
